5eaf136f6b9e77fbbcf31c71e5edd8828a8b65db
kaihatsuRONIT <roniitkhanna@gmail.com>
2025-11-23 17:00:49 +0530

    dataTypes added

---
+++ dataTypes.js
@@ -0,0 +1,32 @@
+//primitive
+//String,Number,bigint,symbol,boolean,null,undefined
+
+//non primitive
+//arrays,objects,functions
+
+const nums = ["hello","world"]
+const myObj = {
+    name:"hello",
+    role:"world",
+    type:78
+}
+const num = 2
+const myFcuk = function(num) {
+    num+=2
+    return num
+}
+console.log(typeof null)
+
+// type of 
+
+// number = number
+//string = string
+//boolean  = boolean
+//null = object
+//undefined = undefined
+//object = object
+//array = object 
+//function = function
+
+//if you log type of unknown variable, it will show undefined
+//but if you log unknown variable, it will throw an error
