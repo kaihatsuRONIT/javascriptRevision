48fa53522b6c3324746a1cc3931b95010c5e00df
kaihatsuRONIT <147860674+kaihatsuRONIT@users.noreply.github.com>
2025-11-23 08:11:18 +0000

    javascript basics

---
+++ basics/dataTypes.js
@@ -0,0 +1,16 @@
+const num0 = 283228271732  //number
+const num1 = 83242938195731985n //bigint
+const num2 = "82834825819515" //string
+const num3 = true  //boolean
+const num4 = {a:318,b:382} //object
+const num5 = null; //also object
+let num6; //undefined
+console.table([
+    typeof(num0),
+    typeof(num1),
+    typeof(num2),
+    typeof(num3),
+    typeof(num4),
+    typeof(num5),
+    typeof(num6)
+])
---
+++ basics/variables.js
@@ -0,0 +1,7 @@
+const personName = "ronitKhanna" //correct
+let age = 21 //correct
+var email = "ronit@khanna.com" //do not use because of scope issues
+marks = 1100 //let or const is must
+let niche;  //correct
+console.log(personName);
+console.table([age,email,marks,niche]);
