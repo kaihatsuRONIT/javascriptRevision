4ef35c2559a4e7446b93ab4057307bb113b2f9a3
kaihatsuRONIT <roniitkhanna@gmail.com>
2025-11-25 14:34:46 +0530

    number and math addes

---
+++ basics/numberAndMath.js
@@ -0,0 +1,14 @@
+const num = 38;
+const numb = new Number(83239502)
+console.log(numb);
+console.log(num);
+
+console.log(Number.MIN_VALUE)
+console.log(numb.toLocaleString("en-IN"))
+
+//creating a dice
+const min = 1;
+const max = 6;
+
+console.log(Math.floor(Math.random() * (max-min)) + min)
+console.log(19.838.toPrecision(10)) // print total precise values
