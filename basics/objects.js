efde87aedeb598fc2f90e893c52a6970f9189e78
kaihatsuRONIT <roniitkhanna@gmail.com>
2025-11-30 15:01:42 +0530

    deep understanding of objects

--- basics/objects.js
+++ basics/objects.js
@@ -24,4 +24,19 @@ console.log(my_obj) //in this greeting func is not executed but its reference is
 
 Object.freeze(my_obj); //this do not allow futher modification in the object
 
+//part 2
+const new_Obj = {1:"One",2:"Two",3:"Three"}
+const new_Obj_2 = {9:"Nine",8:"Eight"}
 
+// const getObject = Object.assign(new_Obj,new_Obj_2)   //one type to store different objects in single object
+// const getObject = Object.assign({},new_Obj,new_Obj_2)  //store in new object
+const getObject = {...new_Obj,...new_Obj_2}  //best and practical way
+
+
+console.log(Object.keys(new_Obj))  //return array of all keys
+console.log(Object.values(new_Obj_2))  //returns array of all values
+console.log(Object.entries(new_Obj)) //return array of all entries
+
+// const {name,age} = my_obj;  //instead of using my_obj.name, use this format
+// console.log(name)
+// console.log(age)
