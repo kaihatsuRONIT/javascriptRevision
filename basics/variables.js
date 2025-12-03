cf538d5c82e9e608ba44da367f8a940e13555471
kaihatsuRONIT <roniitkhanna@gmail.com>
2025-12-03 15:45:48 +0530

    updated variables.js

--- basics/variables.js
+++ basics/variables.js
@@ -1,7 +1,13 @@
 const personName = "ronitKhanna" //correct
 let age = 21 //correct
 var email = "ronit@khanna.com" //do not use because of scope issues
-marks = 1100 //let or const is must
+marks = 1100 //let or const is must 
 let niche;  //correct
 console.log(personName);
 console.table([age,email,marks,niche]);
+
+{
+    num = 10; //this will act as VAR
+    console.log(num);
+}
+console.log(num) //run without error
